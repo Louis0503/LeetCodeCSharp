025de9cee4abe269cd2156457ce0d0c6eb2d8ce7
LouisLiu <louis19820503@gmail.com>
2016-05-06 15:48:46 +0800

    347

---
+++ 347_Top_K_Frequent_Elements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Top_K_Frequent_Elements
+    {
+        public IList<int> TopKFrequent(int[] nums, int k)
+        {
+            List<int> tmpList = new List<int>();
+            Dictionary<int, int> hashDictionary = new Dictionary<int, int>();
+            foreach (int element in nums){
+                if (hashDictionary.ContainsKey(element)){
+                    hashDictionary[element]++;
+                }else{
+                    hashDictionary.Add(element,1);
+                }
+            }
+
+            tmpList = hashDictionary.OrderByDescending(kp => kp.Value)
+                                    .Select(kp => kp.Key)
+                                    .ToList();
+            int[] result = new int[k];
+            tmpList.CopyTo(0,result,0,k);
+            return result;
+        }
+        
+    }
+}
---
+++ DataStruct/MinHeap.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.DataStruct
+{
+    public class MinHeap<T> where T : IComparable
+    {
+        List<T> elements;
+        public MinHeap()
+        {
+            elements = new List<T>();
+        }
+        public void Add(T item)
+        {
+            elements.Add(item);
+            Heapify();
+        }
+        public void Delete(T item)
+        {
+            int index = elements.IndexOf(item);
+            int last = elements.Count - 1;
+            elements[index] = elements[last];
+            elements.RemoveAt(last);
+            Heapify();
+        }
+
+        public T PopMin()
+        {
+            if (elements.Count > 0){
+                T item = elements[0];
+                Delete(item);
+                return item;
+            }
+            return default(T);
+        }
+
+
+        #region Help Methods
+        private void Heapify()
+        {
+            for (int i = elements.Count - 1; i > 0; i--){
+                int parentPosition = (i + 1) / 2 - 1;
+                parentPosition = parentPosition >= 0 ? parentPosition : 0;
+
+                if (elements[parentPosition].CompareTo(elements[i]) > 1){
+                    SwapElements(parentPosition, i);
+                }
+            }
+        }
+
+        private void SwapElements(int leftIndex, int rightIndex)
+        {
+            T tmp = elements[leftIndex];
+            elements[leftIndex] = elements[rightIndex];
+            elements[rightIndex] = tmp;
+        }
+
+        #endregion
+    }
+}
