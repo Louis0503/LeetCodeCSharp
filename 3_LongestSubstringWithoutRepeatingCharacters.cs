bafc2aa524775700003bb2164ceafd94fb537802
LouisLiu <louis19820503@gmail.com>
2016-10-04 18:25:38 +0800

    Longest Substring Without Repeating Characters

---
+++ 3_LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _3_LongestSubstringWithoutRepeatingCharacters
+    {
+        private int[] _records;
+        public _3_LongestSubstringWithoutRepeatingCharacters()
+        {
+            _records = new int[256];
+        }
+        public int LengthOfLongestSubstring(string s)
+        {
+            FlushRecords();
+            var maxlength = 0;
+            var start = 0;
+
+            for (var i = 0; i < s.Length; i++) {
+                var thisChar = s[i];
+                var index = GetIndex(thisChar);
+
+                if (index >=0) {
+                    if (i - start > maxlength) {
+                        maxlength = i - start;
+                    }
+                    start = index + 1;
+                }
+                _records[thisChar] = i;
+            }
+
+            if (s.Length - start > maxlength) {
+                maxlength = s.Length - start;
+            }
+
+            return maxlength;
+        }
+
+        private int GetIndex(char c)
+        {
+            return _records[c];
+        }
+
+        private void FlushRecords()
+        {
+            for (var i = 0; i < _records.Length; i++) {
+                _records[i] = -1;
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/_3_LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _3_LongestSubstringWithoutRepeatingCharactersTests
+    {
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest()
+        {
+            //arrange
+            var target = new _3_LongestSubstringWithoutRepeatingCharacters();
+            var expects = new int []{ 3, 3, 1, 3 , 10, 2};
+            var inputs = new string[] {"test", "abcabcbb", "bbbbb", "pwwkew","testabcdfgh", "abba"};
+            //act
+            var actuals = new int[6];
+            for (var i = 0; i < inputs.Length; i++) {
+                actuals[i] = target.LengthOfLongestSubstring(inputs[i]);
+            }
+            //assert
+            CollectionAssert.AreEqual(expects, actuals);
+        }
+    }
+}
